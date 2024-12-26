6935093eaeb3ce6d834f39507cf3f0888d8dc215
Minh Andy Nguyen Xuan <minh.x.nguyen@opswat.com>
2024-12-26 11:26:51 +0700

    Innit Winform

---
+++ winform-app/src/MainForm.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace MyWinformApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Initialization logic here
+        }
+
+        // Add event handlers and methods for user interactions here
+    }
